7ae663e686951fc273b8b0495798a12f2bcfe368
rin-dz <dz.rina.na@gmail.com>
2023-11-16 01:15:48 +0300

    dev: добавить проект в портфолио

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,16 +12,15 @@ function AboutMe() {
             <article className="aboutme__article">
                 <div className="aboutme__text-container">
                     <img className="aboutme__article-hidden-img" src={aboutme_png} alt="фото студента" />
-                    <h3 className="aboutme__article-title">Виталий</h3>
+                    <h3 className="aboutme__article-title">Арина</h3>
                     <h4 className="aboutme__article-description">
-                        Фронтенд-разработчик, 30 лет
+                        Фронтенд-разработчик, 20 лет
                     </h4>
                     <p className="aboutme__article-text">
-                        Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня
-                        есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно
-                        начал кодить. С 2015 года работал в компании «СКБ Контур». После того,
-                        как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и
-                        ушёл с постоянной работы.
+                        Я родилась и живу в Москве. Чуть более года назад меня заинтересовала веб-разработка, а затем,
+                        когда по прошествии времени интерес не прошёл, я решила основательно заняться изучением 
+                        этой сферы - именно так я попала на курс веб-разработки от Яндекс.Практикума. Сейчас, когда 
+                        уверенности в собственных знаниях стало больше, я занимаюсь активным поиском работы.
                     </p>
                     <a className="aboutme__article-link link-button" href='https://github.com/rina-dz' target="_blank" rel="noreferrer" >Github</a>
                 </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,8 +14,12 @@ function Portfolio() {
                 <h3 className="portfolio__link-name">Адаптивный сайт</h3>
                 <p className="portfolio__link-icon">↗</p>
             </a>
-            <a className="portfolio__link link-button" href='https://rina-dz.github.io/mesto-react/' target="_blank" rel="noreferrer" >
+            <a className="portfolio__link link-button" href='https://mesto-react-ashy.vercel.app/' target="_blank" rel="noreferrer" >
                 <h3 className="portfolio__link-name">Одностраничное приложение</h3>
+                <p className="portfolio__link-icon">↗</p>
+            </a>
+            <a className="portfolio__link link-button" href='https://react-mesto-auth-five-rosy.vercel.app/sign-in' target="_blank" rel="noreferrer" >
+                <h3 className="portfolio__link-name">Сайт для обмена фотографиями</h3>
                 <p className="portfolio__link-icon">↗</p>
             </a>
         </section>
